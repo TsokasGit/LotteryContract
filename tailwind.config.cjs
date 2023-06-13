fd1fb5c038e597effb063a648b24d58efefe8ae2
Petros Tsokas <43913088+TsokasGit@users.noreply.github.com>
2023-06-13 15:05:19 +0300

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#1F2937',
+        'blacky-black': '#030712',
+      }
+    },
+  },
+  plugins: [],
+}
+
