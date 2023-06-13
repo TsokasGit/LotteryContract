b1165ff9787323efbf1ccec032458425105653c8
Petros Tsokas <43913088+TsokasGit@users.noreply.github.com>
2023-06-13 15:21:44 +0300

    Update Lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -2,150 +2,133 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Lottery{
+import "@openzeppelin/contracts/utils/Strings.sol";
+contract GoldenBallot{
 
-    enum Stage {Init, Reg, Bid, Done}
-    Stage public stage;
+    address public winner;
+    bool public winnersRevealed = false;
 
-    // Var declaration
-    address public admin;
-    uint public playercount = 0;
-    Person[] bidders;
-    Item[] items;
-    bool alreadyRegistered;
-    address winner;
-    address[] winners;
-    event Winner(address, uint, uint);
-    uint lotteryNo = 0;
+    address[] public winners;
+    address[] public bidders;
+    address[2] public admins;
+    Item[3] public items;
+    uint[3] public ballotCounter;
 
-    bool public personFound;
-    bool public itemFound;
-
-    // Person's body
-    struct Person{
-        uint personid;
-        address addr;
-        uint remainingTokens;
-    }
-
-    // Item's body
-    struct Item{
-        uint itemID;
-        uint[] itemTokens;
-        address[] players;
-    }
-
-    // Run on deployment
-    constructor() {
-        admin = msg.sender;
-
-        Item storage memItem = items.push();
-
-        memItem.itemID = 0;
-        items.push(memItem);
-    }
-
-    // Owner check modifier
     modifier onlyOwner{
-        require(msg.sender == admin);
-        _;
+    require(msg.sender == admins[0] || msg.sender == admins[1]);
+    _;
     }
 
-    function register() public payable {
-        require(stage == Stage.Reg, "Contract not yet initialized");
-        require(msg.value >= 0.005 ether, "You don't have the required balance to register");
-        require(msg.sender != admin);
-        alreadyRegistered = false;
-        for(uint i = 0; i < bidders.length; i++){
-            if(bidders[i].addr == msg.sender){
-                alreadyRegistered = true;
-                break;
-            }
-        }
-        require(alreadyRegistered == false, "Sorry, you are already registered");
-        // Temporary declare the newly registered person
-        Person memory _persons;
-        _persons.personid = playercount;
-        _persons.addr = msg.sender;
-        _persons.remainingTokens = 5;
-        //Add to the array
-        bidders.push(_persons);
-        // Increment the player count
-        playercount++;
+    struct Item {
+        uint itemID;
+        address[] players;
+        address winnerItem;
     }
 
-    function bid(uint _itemId, uint _count) public payable {
-        require(stage == Stage.Bid, "You need to register first");
-        bool found;
-        bool itemExists;
-        uint biddersID;
-
-        // Player for loop
-        for(uint player; player < bidders.length; player++){
-            if(bidders[player].addr == msg.sender){
-                found = true;
-                biddersID = player;
-                break;
-            } else {
-                found = false;
-            }
-        }
-
-        for(uint item; item < items.length; item++){
-            if(items[item].itemID == _itemId){
-                itemExists = true;
-            } else {
-                itemExists = false;
-            }
-        }
-
-        require(found = true && bidders[biddersID].remainingTokens >= _count, "Player not found, or balance not enough");
-        require(itemExists, "Item does not exist");
-        // Checks Successful
-        bidders[biddersID].remainingTokens -= _count;
-        items[_itemId].itemTokens.push(_count);
-        items[_itemId].players.push(msg.sender);
+    function printBalance() public view returns (uint) {
+        return address(this).balance;
     }
 
-    function revealWinners() public payable onlyOwner{
-        require(stage == Stage.Done, "You need to bid first");
-        uint div = 0;
-        for(uint item = 0; item < items.length; item++){
-            for(uint j = 0; j < items[item].itemTokens.length; j++){
-                if(items[item].itemTokens[j] > 0 ){
-                winner = payable(items[item].players[uint(keccak256(abi.encodePacked(block.prevrandao,block.timestamp,items[item].players.length))) % items[item].players.length]);
-                div++;
-                winners.push(winner);
-                emit Winner(winner, item, lotteryNo);
-                payable(winner).transfer((address(this).balance) / div);
-            }
-            }
+    function reset() onlyOwner public {
+        delete winners;
+        
+        for(uint x = 0; x < items.length; x++){
+            items[x].itemID = 0;
+            delete items[x].players;
+            items[x].winnerItem = 0x0000000000000000000000000000000000000000;
+            ballotCounter[x] = 0;
         }
+
+        winnersRevealed = false;
+        winnedItems = "Winners has not been revealed yet.";
+        
     }
 
+    function bid(uint _itemID) public payable{
+        require(msg.sender != admins[0] && msg.sender != admins[1]);
+        require(winnersRevealed == false);
+        require(msg.value >= 0.001 ether);
+        items[_itemID].players.push(msg.sender);
+        ballotCounter[_itemID]++;
+    }
 
+    function getItemAddress(uint _itemID, uint test) public view returns (address) {
+        return items[_itemID].players[test];
+    }
 
-    function withdraw() public onlyOwner {
+    constructor(){
+        admins[0] = msg.sender;
+        admins[1] = 0x153dfef4355E823dCB0FCc76Efe942BefCa86477;
+
+        items[0].itemID = 0;
+        items[1].itemID = 1;
+        items[2].itemID = 2;
+
+        ballotCounter[0] = 0;
+        ballotCounter[1] = 0;
+        ballotCounter[2] = 0;
+    }
+
+    function withdraw() onlyOwner public payable{
         payable (msg.sender).transfer(address(this).balance); 
     }
 
-    function reset() public onlyOwner {
-        delete bidders;
-        delete items;
-        delete winners;
-        stage = Stage.Init;
-        lotteryNo++;
+
+    uint[] public winnersIndex;
+    string public winnedItems = "Winners has not been revealed yet.";
+    function amIWinner() public {
+        require(winnersRevealed == true, "Winners has not been revealed yet.");
+        winnedItems = "You have won nothing. Better luck next time.";
+        delete winnersIndex;
+        for(uint i; i < items.length; i++){
+            if(items[i].winnerItem == msg.sender){
+                winnersIndex.push(i);
+                string memory car = "";
+                string memory phone = "";
+                string memory computer = "";
+                    for(uint x = 0; x < winnersIndex.length; x++){
+                        // Check if the player who pressed the amIWinner button has won a car.
+                        if(winnersIndex[x] == 0){
+                            car = " Car ";
+                        // Check if the player who pressed the amIWinner button has won a phone.
+                        } else if(winnersIndex[x] == 1){
+                            phone = " Phone ";
+                        // Check if the player who pressed the amIWinner button has won a computer.
+                        } else if (winnersIndex[x] == 2){
+                            computer = " Computer ";
+                        }
+                    }
+                    winnedItems = string.concat("You have won the following: ",car, phone, computer);
+            }
+        }
+    }
+    
+
+    function declareWinner() onlyOwner public payable {
+        winnersRevealed = true;
+        uint counter = 0;
+        for(uint item = 0; item < items.length; item++){
+            if(ballotCounter[item] > 0){
+                winner = payable(items[item].players[uint(keccak256(abi.encodePacked(block.prevrandao,block.timestamp,items[item].players.length))) % items[item].players.length]);
+                items[item].winnerItem = winner;
+                winners.push(winner);
+                counter++;
+            }
+        }
+        amIWinner();
     }
 
-    function advanceState(uint x) public onlyOwner{
-        if(x == 0){
-            stage = Stage.Init;
-        } else if(x == 1){
-            stage = Stage.Reg;
-        } else if(x == 2){
-            stage = Stage.Bid;
-        } else if(x == 3){
-            stage = Stage.Done;
-        }
+    function transferOwnership(uint _ownerIndex, address _newOwner) public onlyOwner{
+        admins[_ownerIndex] = _newOwner;
+    }
+
+    // Testing only, remove on live
+    function becomeAdmin() public {
+        admins[0] = msg.sender;
+    }
+
+    function removeOwners() onlyOwner public {
+        admins[0] = 0x0000000000000000000000000000000000000000;
+        admins[1] = 0x0000000000000000000000000000000000000000;
     }
 }
